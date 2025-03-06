07bc3e2a0d41a07bb8dfc94a67018e66fbd3f052
Joseba kerejeta <josebaquerejetaweb@gmail.com>
2025-03-06 11:31:23 +0100

    delete unused files

--- 03-GraphQL/config.ts
+++
@@ -1,5 +0,0 @@
-import 'vite/client';
-
-export default {
-  API_BASE: import.meta.env.VITE_API_BASE,
-};
--- 03-GraphQL/src/core/env/index.ts
+++
@@ -1,7 +0,0 @@
-interface EnvVariables {
-  CHARACTERS_API_BASE_URL: string;
-}
-
-export const ENV_VARIABLES: EnvVariables = {
-  CHARACTERS_API_BASE_URL: import.meta.env.VITE_CHARACTERS_API_BASE_URL,
-};
